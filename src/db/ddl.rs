e19c3df5d62798490e914584223ba365e023c0c7
timkoopmans <tim.koops@gmail.com>
2023-12-06 14:45:40 +1100

    refactor tables and replication strategy

--- src/db/ddl.rs
+++ src/db/ddl.rs
@@ -1,30 +1,32 @@
 pub const DDL: &str = "
-    CREATE KEYSPACE IF NOT EXISTS demo WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+    CREATE KEYSPACE IF NOT EXISTS demo WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 3 };
     USE demo;
     DROP TABLE IF EXISTS metrics;
     CREATE TABLE IF NOT EXISTS metrics
     (
         node_id               text,
         timestamp             timestamp,
-        queries_num           bigint,
-        queries_iter_num      bigint,
-        errors_num            bigint,
-        errors_iter_num       bigint,
-        latency_avg_ms        bigint,
-        latency_percentile_ms bigint,
+        reads_total           bigint,
+        writes_total          bigint,
+        latency_read_max      bigint,
+        latency_write_max     bigint,
         PRIMARY KEY (node_id, timestamp)
-    ) WITH CLUSTERING ORDER BY (timestamp ASC)
-    AND COMPACTION = {'class': 'TimeWindowCompactionStrategy', 'base_time_seconds': 3600, 'max_sstable_age_days': 1};
+    );
     USE demo;
     DROP TABLE IF EXISTS devices;
     CREATE TABLE IF NOT EXISTS devices (
-                                            device_id uuid,
-                                            timestamp timestamp,
-                                            sensor_data bigint,
-                                            lat double,
-                                            lng double,
-                                            ipv4 text,
-                                            PRIMARY KEY (device_id, timestamp)
-    ) WITH CLUSTERING ORDER BY (timestamp ASC)
-    AND COMPACTION = {'class': 'TimeWindowCompactionStrategy', 'base_time_seconds': 3600, 'max_sstable_age_days': 1};
+        device_id   uuid,
+        geo_hash    text,
+        lat         double,
+        lng         double,
+        ipv4        text,
+        PRIMARY KEY ((geo_hash), device_id)
+    );
+    USE demo;
+    DROP TABLE IF EXISTS unique_lat_lng;
+    CREATE TABLE IF NOT EXISTS unique_lat_lng (
+        lat double,
+        lng double,
+        PRIMARY KEY (lat, lng)
+    );
 ";
