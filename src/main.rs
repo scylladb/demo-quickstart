7f1c27e24cfc605d31d8c973474089cca23b89ec
timkoopmans <tim.koops@gmail.com>
2023-12-13 07:39:28 +1100

    - shared db session - concurrent simulator

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use crate::db::connection;
 use crate::util::devices;
 use anyhow::anyhow;
 use std::sync::Arc;
+use futures::stream::FuturesUnordered;
+use futures::TryStreamExt;
 use structopt::StructOpt;
-use tokio::{task, try_join};
+use tokio::task;
 use util::metrics;
 use web::server;
 use crate::util::buffer::{flush_buffer_to_db, LatLongBuffer};
@@ -20,6 +22,9 @@ pub struct Opt {
     /// write ratio
     #[structopt(default_value = "80")]
     write_ratio: u8,
+    /// simulators
+    #[structopt(default_value = "50")]
+    simulators: u8,
 }
 
 #[tokio::main]
@@ -34,44 +39,40 @@ async fn main() -> Result<(), anyhow::Error> {
         ));
     }
 
-    let metrics_session = Arc::new(
+    let db_session = Arc::new(
         connection::builder(true)
             .await
             .expect("Failed to connect to database"),
     );
 
-    let devices_session = Arc::new(
-        connection::builder(true)
-            .await
-            .expect("Failed to connect to database"),
-    );
-
-    let web = server::init(metrics_session.clone(), opt.clone()).await;
+    let web = server::init(db_session.clone(), opt.clone()).await;
     tokio::spawn(async { web.launch().await.unwrap() });
 
     let metrics_task = task::spawn(metrics::worker(
-        metrics_session.clone(),
+        db_session.clone(),
     ));
 
     let lat_long_buffer = Arc::new(LatLongBuffer::new());
-    let devices_task = task::spawn(devices::simulator(
-        devices_session.clone(),
-        Arc::clone(&lat_long_buffer),
-        opt.read_ratio,
-        opt.write_ratio,
-    ));
-
     let flush_task = task::spawn(flush_buffer_to_db(
-        devices_session.clone(),
+        db_session.clone(),
         Arc::clone(&lat_long_buffer),
     ));
 
-    // Wait for all tasks to complete
-    let (metrics_result, devices_result, flush_result) = try_join!(metrics_task, devices_task, flush_task)?;
+    let devices_tasks: FuturesUnordered<_> = (0..opt.simulators).map(|_| {
+        task::spawn(devices::simulator(
+            db_session.clone(),
+            Arc::clone(&lat_long_buffer),
+            opt.read_ratio,
+            opt.write_ratio,
+        ))
+    }).collect();
+    let devices_result = devices_tasks.try_collect::<Vec<_>>().await?;
 
+    let (metrics_result, flush_result) = tokio::try_join!(metrics_task, flush_task)?;
     metrics_result?;
-    devices_result?;
     flush_result?;
+
+    devices_result.into_iter().collect::<Result<Vec<_>, _>>()?;
 
     Ok(())
 }
