d72400f7f7a059c5e42efae58215052903a7d0ce
timkoopmans <tim.koops@gmail.com>
2024-06-19 15:36:58 +1000

    leave structs here

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,5 +1,6 @@
 use rocket::serde::Serialize;
 use scylla::FromRow;
+use uuid::Uuid;
 
 #[derive(Clone, Debug, Serialize, FromRow)]
 pub struct Metric {
@@ -9,4 +10,26 @@ pub struct Metric {
     pub latency_write_max: i64,
     pub reads_total: i64,
     pub writes_total: i64,
+}
+
+#[derive(Serialize)]
+pub struct RateMetric {
+    pub node_id: String,
+    pub timestamp: i64,
+    pub reads_per_second: f64,
+    pub writes_per_second: f64,
+    pub ops_per_second: f64,
+    pub reads_total: i64,
+    pub writes_total: i64,
+    pub latency_read_max: f64,
+    pub latency_write_max: f64,
+}
+
+#[derive(Clone, Debug, FromRow, Serialize)]
+pub struct Device {
+    pub device_id: Uuid,
+    pub geo_hash: String,
+    pub ipv4: String,
+    pub lat: f64,
+    pub lng: f64,
 }
