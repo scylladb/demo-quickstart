9ebf2d9f4c181207fee182e663cd9ea95ebb808e
timkoopmans <tim.koops@gmail.com>
2024-07-11 10:16:54 +1000

    lose the drop statements so somebody demo'ing can modify the once run schema.

--- src/db/ddl.rs
+++ src/db/ddl.rs
@@ -1,7 +1,6 @@
 pub const DDL: &str = "
     CREATE KEYSPACE IF NOT EXISTS demo WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : <RF> };
     USE demo;
-    DROP TABLE IF EXISTS metrics;
     CREATE TABLE IF NOT EXISTS metrics
     (
         node_id               text,
@@ -13,7 +12,6 @@ pub const DDL: &str = "
         PRIMARY KEY (node_id, timestamp)
     );
     USE demo;
-    DROP TABLE IF EXISTS devices;
     CREATE TABLE IF NOT EXISTS devices (
         device_id   uuid,
         geo_hash    text,
